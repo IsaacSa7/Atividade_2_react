d0d7ad0bd2949b31d4cf57d1dc9eaab4a477b407
IsaacSa7 <isaac.sa.silva08@aluno.ifce.edu.br>
2023-12-06 22:40:39 -0300

    subindo atualização

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import MyNavbar from './components/nav'
 import Dashboard from './pages/dashboard'
-import Cadastro from './pages/cadastro'
+import Cadastro from './pages/cadastro/index'
+import Edit from './pages/cadastro/Edit'
 
 const RoutesAPP = () => {
   return (
@@ -13,6 +14,8 @@ const RoutesAPP = () => {
         <Routes>
             <Route path='/' element={<Dashboard/>} />
             <Route path='/cadastro' element={<Cadastro/>}/>
+            <Route path='/edit' element={<Edit/>}/>
+
         </Routes>
     </BrowserRouter>
 
